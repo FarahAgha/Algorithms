1a93a39ff5c589c46ee4cd6aabba2435d8c0b112
= <=>
2017-08-14 12:14:01 -0500

    Bubble Sort algo

---
+++ src/grokking/BubbleSort.java
@@ -0,0 +1,19 @@
+package grokking;
+
+public class BubbleSort {
+    public static void sort(Integer[] array) {
+        for (int i = 0; i < array.length ; i++) {
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j - 1] > array[j]) {
+                    swap(array, j, j - 1);
+                }
+            }
+        }
+    }
+
+    private static void swap(Integer[] array, int a, int b) {
+        Integer temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+}
